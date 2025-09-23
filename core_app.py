117774f557c8608c91c2b7be71d1aa39d199c99e
DaanVinck <168782967+DaanVinck@users.noreply.github.com>
2025-09-23 13:31:48 +0200

    v1

--- core_app.py
+++ core_app.py
@@ -1,3 +1,7 @@
+import streamlit as st
+from functions import login
+from functions import acc
+from functions import home
 from dotenv import load_dotenv 
 load_dotenv()
 import os
@@ -8,6 +12,36 @@ key = os.environ.get("SUPABASE_KEY")
 supabase = create_client(url, key)
 
 
-#data = supabase.table("test_investia").insert({"id": "Daan", "pasword": "123456", "email": "daan@gmail.com"}).execute()
-data = supabase.table("test_investia").select("*").execute()
-print(data)
+
+data = supabase.table("test_investia").select("id, pasword").execute()
+
+for item in data:
+    for i in item:
+        if type(i) == list:
+            mylist = i
+name_list = []
+code_dict = {}
+for item in mylist:
+    name_list.append(item.get('id'))
+    code_dict[item.get('id')] = (item.get('pasword'))
+
+# Initialiseer de login state als die nog niet bestaat
+if 'logged_in' not in st.session_state:
+    st.session_state['logged_in'] = False
+
+# Toon de juiste pagina
+if st.session_state['logged_in'] == 'home':
+    home(st.session_state['naam'])
+elif st.session_state['logged_in'] == 'new_acc':
+    name, email, code = acc(name_list)
+    if not name or name == "bla" or not code or code == "bla" or not email or email == "bla":
+        st.error("Vul alstublieft alle velden in!")
+    elif name in name_list:
+        st.error("je zit al in het systeem!")
+    else:
+        res = supabase.table("test_investia").insert([{"id": name, "pasword": code, "email": email}]).execute()
+        st.session_state['logged_in'] = 'home'
+        st.session_state['naam'] = name
+else:
+    name = login(name_list, code_dict)
+    st.session_state['naam'] = name
--- test.py
+++ test.py
@@ -1,7 +1,19 @@
-from dotenv import load_dotenv
-import os
 
+from dotenv import load_dotenv 
 load_dotenv()
+import os
+from supabase import create_client, Client
 
 url = os.environ.get("SUPABASE_URL")
-print("SUPABASE_URL:", url)
+key = os.environ.get("SUPABASE_KEY")
+supabase = create_client(url, key)
+
+name = "Daansqmfqs"
+code = "1234"
+email = "daan@example.com"
+
+# Voeg het toe aan de tabel
+res = supabase.table("test_investia").insert([{"id": name, "pasword": code, "email": email}]).execute()
+data = supabase.table("test_investia").select("id, pasword").execute()
+
+print(data)
